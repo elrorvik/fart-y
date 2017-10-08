b88c6877d500772892bed3b754233e3f113b28c0
Ella-Lovise H. Rørvik <elh.rorvik@gmail.com>
2017-10-08 23:10:11 +0200

    made folders

---
+++ assignment_2/code/run_task_2f/run_task_2f.m
@@ -0,0 +1,36 @@
+close all;
+clear all;
+clc;
+
+parameters_longitudal_autopilot; % get parameters
+d = 2*deg2rad;
+regulation_time = 100;
+t_end = 3*regulation_time;
+
+time = 0:1:t_end-1;
+
+chi_c_part_1 = 5*deg2rad.*ones(1, regulation_time)';
+chi_c_part_2 = 15*deg2rad.*ones(1, regulation_time)';
+chi_c_part_3 = 0*deg2rad.*ones(1, regulation_time)';
+chi_c = timeseries([chi_c_part_1; chi_c_part_2; chi_c_part_3]');
+
+model = 'lateral_autopilot_full.slx';
+load_system(model);
+simOut = sim(model);
+
+figure(1)
+subplot(311)
+plot(chi.time,chi.signals.values.*rad2deg); hold on; 
+plot(chi_c.time, [chi_c_part_1; chi_c_part_2; chi_c_part_3].*rad2deg);  hold on;
+legend('\chi', '\chi_c'); 
+title('Simulation of \chi'); xlabel('time (s)'),ylabel('deg'),grid
+
+subplot(312)
+plot(phi.time,phi.signals.values.*rad2deg); hold on; 
+plot(phi_c.time, phi_c.signals.values*rad2deg);  hold on; axis([0 t_end -90 50]);
+legend('\phi', '\phi_c'); 
+title('Simulation of \phi'); xlabel('time (s)'),ylabel('deg'),grid
+
+subplot(313)
+plot(delta_a_c.time,delta_a_c.signals.values.*rad2deg); hold on; axis([0 t_end -35 35]);
+title('Simulation of \delta_a^c'); xlabel('time (s)'),ylabel('deg'),grid
