08f3963e3e9910f7125fe9b3d0772345541b7f2d
Ella-Lovise H. Rørvik <elh.rorvik@gmail.com>
2017-10-08 17:04:17 +0200

    stupid ella

--- assignment_2/code/run_task_2g.m
+++ assignment_2/code/run_task_2g.m
@@ -5,7 +5,7 @@ clc;
 parameters_longitudal_autopilot; % get parameters
 %change !!!!!
 chi_c  = 10*deg2rad;
-d = 30*deg2rad;
+d = 40*deg2rad;
 regulation_time = 100;
 t_end = 3*regulation_time + regulation_time;
 
@@ -18,24 +18,37 @@ chi_c_part_2 = 15*deg2rad.*ones(1, regulation_time)';
 chi_c_part_3 = 0*deg2rad.*ones(1, regulation_time)';
 chi_c = timeseries([chi_c_part_1; chi_c_part_2; chi_c_part_3]');
 
+
 model = 'lateral_autopilot_full_2g.slx';
-load_system(model);
-simOut = sim(model);
+Legend = {};
 
-subplot(311)
-plot(chi.time,chi.signals.values.*rad2deg); hold on; 
-plot(chi_c.time, [chi_c_part_1; chi_c_part_2; chi_c_part_3].*rad2deg); hold on;
-legend('\chi', '\chi_c');
+i  = 1;
+for integrator_error_limit = [0*deg2rad, 1*deg2rad, 5*deg2rad]
+    Legend(i) = {strcat('IEL = ', num2str(integrator_error_limit*rad2deg))};
+    i = i+1;
+    load_system(model);
+    simOut = sim(model);
+
+    subplot(311)
+    plot(chi.time,chi.signals.values.*rad2deg); hold on;
+
+    subplot(312)
+    plot(delta_a_c.time,delta_a_c.signals.values.*rad2deg); hold on; 
+
+    subplot(313)
+    plot(integrator.time, integrator.signals.values.*rad2deg);  hold on;
+end
 
 subplot(312)
-plot(delta_a_c.time,delta_a_c.signals.values.*rad2deg); hold on; axis([0 t_end -35 35]);
-
-subplot(313)
-plot(integrator.time, integrator.signals.values.*rad2deg);  hold on;
-
-subplot(311)
-title('Simulation of \chi')
-subplot(312)
+legend(Legend); axis([0 t_end -35 35]);
 title('Simulation of \delta')
+
 subplot(313)
+legend(Legend);
 title('The saturated integrator values')
+
+subplot(311)
+plot(chi_c.time, [chi_c_part_1; chi_c_part_2; chi_c_part_3].*rad2deg); hold on;
+Legend_2 = cat(2,Legend,{'chi_c'});
+legend(Legend_2);
+title('Simulation of \chi')
