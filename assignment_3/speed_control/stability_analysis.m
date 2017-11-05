2cd20e2231c50012bfb4f2724778398dc22b954a
Ella-Lovise H. Rørvik <elh.rorvik@gmail.com>
2017-11-05 16:05:47 +0100

    working on comparinson between speed modell and MSfartøy

--- assignment_3/speed_control/stability_analysis.m
+++ assignment_3/speed_control/stability_analysis.m
@@ -2,7 +2,6 @@
 solve_diff ; % finds nomoto speed coefficients
 w_n = 1;
 lambda = 1;
-w_b = w_n*sqrt(1-lambda^2 + sqrt(4*lambda^4 - 4*lambda^2 +2))
 K_u = K_v;
 T_u = K_v;
 
@@ -13,7 +12,7 @@ Kd = 0.2;
 s = tf('s');
 
 H_u = K_u*(1+ T_u*s)/((1+T1*s)*(1+T2*s));
-H_r = Kp + Ki/s + Kd*s;
+H_r = pid(Kp,Ki,Kd);
 
 H_1 = H_u*H_r;
 margin(H_u);
--- assignment_3/speed_control/system_identification.m
+++ assignment_3/speed_control/system_identification.m
@@ -13,8 +13,12 @@ c   = 0;                    %Current on (1)/off (0)
 
 addpath(genpath('../header_control'));
 parameters_heading_controller
+delta_max = 25*pi/180;
+delta_min = -delta_max;
+psi_amp = 0;
+psi_freq = 0.01;
 
-nc_list = [0 20 40 65 85]*2*pi/60;
+nc_list = [40 65 85]*2*pi/60;
 data_matrix = zeros(length(nc_list),7);
 pid_matrix = zeros(length(nc_list),7);
 for i = 1:length(nc_list)
@@ -23,15 +27,69 @@ for i = 1:length(nc_list)
     u = v(:,1);
     nc_vec = nc*ones(length(u),1);
     data = iddata(u,nc_vec,tsamp);
-    sys = tfest(data,2);
+    sys = tfest(data,1);
+    [Wn,zeta] = damp(sys);
     sys_report = sys.Report;
     fit = sys_report.Fit.FitPercent;
-    data_matrix(i,:) = [nc,sys.Numerator, sys.Denominator, fit];
+    data_matrix(i,:) = [nc,sys.Numerator, sys.Denominator, fit,Wn, zeta];
     [C,info] = pidtune(sys,'PID');
     pid_matrix(i,:) = [nc, C.Kp, C.Kd, C.Ki,info.Stable, info.CrossoverFrequency, info.PhaseMargin];
 end
 
+%% comparinson with fartøy pid
 
+v0  = [4 0]';            %Initial velocity (body)[m/s]
+clf;
+figure(1); hold on; xlabel('time [s]'); ylabel('speed [m/s]'); 
+title('Comparing modell and ship','FontSize',12);
+Legend = {};
+ustepend = 7;
+e_u_limit = 100000;
+tstep = 0;
+tstop = 10000;
+tstart = 0;
+ustep0 = v0(1);
+Kp_u = 80; %500*wn^2*T/K 
+Ki_u = 0.08; %1000*wn^3*T/(10*K) 
+Kd_u = 0;
+
+wn_u_list = [2,0.05, 0.5]; % not needed on 1
+zeta_u_list = [0.8,2,1];
+nc_gain_list = [1.5, 0.05, 1];
+Ki_u_gain_list = [1,0.3, 1]; % 1 means not done
+for i = 3%1:1:length(nc_list)
+    c = pid_matrix(i,:);
+    if i ==1
+        Ki_u_est = c(4)*0.7;
+    end
+    Ki_u_est = c(4)*0.7;
+    Kd_u_est = c(3);
+    Kp_u_est = c(2);
+    
+    Ki_u = c(4)*Ki_u_gain_list(i);
+    Kp_u_ = c(2);
+    nc_gain = nc_gain_list(i);
+    
+    wn_u = wn_u_list(i);
+    zeta_u = zeta_u_list(i);
+    
+    den =data_matrix(i,2);
+    num= data_matrix(i,3:4);
+
+    sim 'comparing_modell_MSfartoy';
+    u = v(:,1);
+    %h_u = tf(data_matrix(i+2,2), data_matrix(i+2,3:4));
+    %h_r = pid(Kp,Ki, Kd);
+    %sys = 1/(h_u*h_r +1);
+    %opt = stepDataOptions('InputOffset',4,'StepAmplitude',5);
+    %[u_est,t_est] = step(sys); 
+    figure(1);
+    plot(t,u,'--'); hold on;
+    plot(t,u_est); hold on;
+    Legend(2*i-1) = {strcat('Ship, n_c = ', int2str(nc_list(i)), ' rpm')};
+    Legend(2*i)   = {strcat('Modell, n_c = ', int2str(nc_list(i)), ' rpm')}; 
+end
+legend(Legend);
 
 
 
