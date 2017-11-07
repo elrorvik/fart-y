097e7625323519bb38d09f6282396710fc3b9c34
Ella-Lovise H. Rørvik <elh.rorvik@gmail.com>
2017-11-07 13:34:33 +0100

    nice plotting

--- assignment_3/LOS_crab/run_task_2_5.m
+++ assignment_3/LOS_crab/run_task_2_5.m
@@ -1,6 +1,13 @@
 clear;
 close all;
 
+set(0,'DefaultLineLineWidth',2);
+set(0,'DefaultLegendFontSize',16);
+set(0,'defaultAxesFontSize',11)
+set(0,'defaultTextInterpreter','latex');
+set(0,'defaultLegendInterpreter','latex');
+
+
 addpath(genpath('../header_control'));
 parameters_heading_controller; % pick up parameters
 
@@ -26,6 +33,7 @@ Ki_u = 0.08; %1000*wn^3*T/(10*K)
 Kd_u = 0;
 e_u_limit = 3;
 nc_max = (85*2*pi)/60;
+delta_max = 25*deg2rad;
 
 L = 304.8;  %m
 Delta = 2.5*L;
@@ -41,8 +49,7 @@ r_d = 0*deg2rad;
 tstep = 500;
 ustep0 = 4;
 ustepend = 7;
-u_d_vec = 4*ones(1, tstop);
-u_d_vec(tstep:end) = 7*ones(1,tstop-tstep+1);
+
 w_c = 0.01;
 % referance model
 zeta = 1;
@@ -63,16 +70,19 @@ tstop = t(end);
 pathplotter(x, y,  psi, tsamp, 2, tstart, tstop, 0, wp)
 
 figure(3);
-subplot(221); plot(t,u), title('u'); xlabel('s');ylabel('m/s');
-subplot(223); plot(t,nc); title('nc'); hold on; xlabel('s');ylabel('rad/s');
-subplot(222); plot(t,psi*rad2deg); title('\psi'); hold on; xlabel('s');ylabel('degrees');
-plot(t, psi_d*rad2deg), legend('\psi', '\psi_d');
-subplot(224); plot(t,delta_c*rad2deg); title('\delta_c'); hold on; xlabel('s'); ylabel('degrees');
+subplot(221); plot(t,u), title('u','FontSize',12); xlabel('time[s]');ylabel('velocity [m/s]'); hold on;
+subplot(221); plot(t,u_d); legend('u','$u_d$');
+subplot(223); plot(t,nc*rad2deg);  title('$n_c$','FontSize',12);  xlabel('time [s]');ylabel('shaft speed [deg/s]'); hold on;
+subplot(222); plot(t,psi*rad2deg); title('$\psi$','FontSize',12);xlabel('time [s]');ylabel('angle [deg]'); hold on;
+subplot(222); plot(t, psi_d*rad2deg); hold on;
+subplot(222); plot(t,psi_tilde*rad2deg);legend('$\psi$', '$\psi_d$','$\tilde{\psi}_d$'); hold on;
+subplot(224); plot(t,delta_c*rad2deg); title('$\delta_c$','FontSize',12); xlabel('time [s]'); ylabel('angle [deg]'); hold on;
 %subplot(224); plot(t,dc*rad2deg); title('dc'); hold on; legend('kp*e','ki*e','kd*e');
 
-figure(4);
-subplot(221); plot(t,beta*rad2deg); hold on; xlabel('s'); ylabel('degrees');
-subplot(222);plot(t,chi*rad2deg);
-subplot(222); plot(t,chi_d*rad2deg); legend('\chi', '\chi_d');
+figure(4);hold on;
+subplot(211); plot(t,beta*rad2deg); title('$\beta$','FontSize',12);hold on; xlabel('time [s]'); ylabel('angle [deg]');
+subplot(212);plot(t,psi*rad2deg); title('$\chi$ and $\psi$','FontSize',12);hold on;
+subplot(212);plot(t,chi*rad2deg); xlabel('time [s]'); ylabel('angle [deg]');  hold on;
+subplot(212); plot(t,chi_d*rad2deg); legend('$\psi$','$\chi$', '$\chi_d$'); hold on;
 
 
