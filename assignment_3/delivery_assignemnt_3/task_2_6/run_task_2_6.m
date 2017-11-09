9926f1b778c8ce9445c6dee29b8bb472734144d8
Ella-Lovise H. Rørvik <elh.rorvik@gmail.com>
2017-11-09 21:45:41 +0100

    new zip deliverry

---
+++ assignment_3/delivery_assignemnt_3/task_2_6/run_task_2_6.m
@@ -0,0 +1,64 @@
+clear;
+close all;
+
+set(0,'DefaultLineLineWidth',2);
+set(0,'DefaultLegendFontSize',16);
+set(0,'defaultAxesFontSize',11)
+set(0,'defaultTextInterpreter','latex');
+set(0,'defaultLegendInterpreter','latex');
+
+
+addpath(genpath('../controll_parameters'));
+parameters_heading_controller; 
+parameters_speed_controller;
+
+deg2rad = pi/180;
+rad2deg = 180/pi;
+
+tstart=0;           % Sim start time
+tstop=20000;        % Sim stop time
+tsamp=10;           % Sampling time for how often states are stored. (NOT ODE solver time step)
+                
+p0=[1500 500]';      % Initial position (NED)
+v0=[6.63 0]';       % Initial velocity (body)
+psi0=50*deg2rad;             % Inital yaw angle
+r0=0;               % Inital yaw rate
+c=1;                % Current on (1)/off (0)
+
+L = 304.8;  %m
+Delta = 2.5*L;
+Kp_los = 1/Delta;
+R_change_wp = 500;  %m
+wp = load('WP.mat');
+wp = [p0 wp.WP];
+
+sim MSFartoystyring_2_6 
+
+u = v(:,1);
+v = v(:,2);
+x = p(:,1);
+y = p(:,2);
+tstop = t(end);
+
+pathplotter(x, y,  psi, tsamp, 2, tstart, tstop, 0, wp)
+
+figure(3);
+subplot(221); plot(t,u), title('u','FontSize',12); xlabel('time[s]');ylabel('velocity [m/s]'); hold on;
+subplot(221); plot(t,u_d); legend('u','$u_d$');
+subplot(223); plot(t,nc*rad2deg);  title('$n_c$','FontSize',12);  xlabel('time [s]');ylabel('shaft speed [deg/s]'); hold on;
+subplot(222); plot(t,psi*rad2deg); title('$\psi$','FontSize',12);xlabel('time [s]');ylabel('angle [deg]'); hold on;
+subplot(222); plot(t, psi_d*rad2deg); hold on;
+subplot(222); plot(t,psi_tilde*rad2deg);legend('$\psi$', '$\psi_d$','$\tilde{\psi}_d$'); hold on;
+subplot(224); plot(t,delta_c*rad2deg); title('$\delta_c$','FontSize',12); xlabel('time [s]'); ylabel('angle [deg]'); hold on;
+
+
+figure(4);hold on;
+subplot(211); plot(t,beta*rad2deg); title('$\beta$','FontSize',12);hold on; xlabel('time [s]'); ylabel('angle [deg]');
+subplot(212);plot(t,psi*rad2deg); title('$\chi$ and $\psi$','FontSize',12);hold on;
+subplot(212);plot(t,chi*rad2deg); xlabel('time [s]'); ylabel('angle [deg]');  hold on;
+subplot(212); plot(t,chi_d*rad2deg); legend('$\psi$','$\chi$', '$\chi_d$'); hold on;
+ 
+
+
+
+
